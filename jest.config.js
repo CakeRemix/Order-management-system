fe2dca99cdef0804985773313092bde2faee728e
cakeremix <Hassan.moahmed@student.giu-uni.de>
2025-12-20 20:54:08 +0200

    Remove obsolete system architecture documentation and Jest configuration; add new polling service implementation with caching, exponential backoff, and metrics tracking.

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  setupFilesAfterEnv: ['./jest.setup.js'],
-  testMatch: ['**/tests/**/*.test.js'],
-  verbose: true
-};
